6816509d4d4bdd39c0371c6b6470ebac2d02d9ae
Anil Vishwakarma <83838666+cyber-teamguru@users.noreply.github.com>
2023-12-21 12:08:07 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ import logo2 from "../Assets/logo2.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
   AiOutlineHome,
